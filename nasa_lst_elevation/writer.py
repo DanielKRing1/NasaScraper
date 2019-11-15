6480e0e9c733f9ea48591ea3cd67128f6c18d9ac
DanielKRing1 <DanielKRing1@gmail.com>
2019-11-14 17:34:55 -0800

    Added python script to write LST_Elevation data to disk as a json file

---
+++ nasa_lst_elevation/writer.py
@@ -0,0 +1,89 @@
+# Reads files that were downloaded to nasa_lst_elevation/data
+# and writes relevant data to a dictionary object
+# which is serialized to disk
+
+import json
+import tables as tb
+import os 
+
+directory = "C:\\Users\\Danie\\Desktop\\Topics\\Bots\\nasa\\nasa_lst_elevation\\data"
+
+files = os.listdir(directory)
+
+
+BATCH_SIZE = 500
+
+def processFiles():
+    totalFiles = len(files)
+    fileNum = 0
+    
+    elevationData = {}
+    
+    while(fileNum < totalFiles):
+        try:
+            hdf5 = openFile(fileNum)
+            data = parseData(hdf5)
+            hdf5.close();
+        
+            addDataToObj(data, elevationData, fileNum)
+        except:
+            print("ERROR in file ", fileNum)
+            
+        fileNum += 1
+        
+    # Done
+    with open('C:\\Users\\Danie\\Desktop\\Topics\\Bots\\nasa\\nasa_lst_elevation\\elevation-data.json', 'w') as outfile:
+        json.dump(elevationData, outfile)
+        
+    print("Done!")
+
+def openFile(fileNum):
+#     print(fileNum / len(files) * 100, "%")
+
+    file = files[fileNum];
+    return tb.open_file(directory + "\\" + file)
+            
+def parseData(fileData):
+#     print("(Lat, Long)\n\n")
+
+    swLong = fileData.root.Geolocation.Longitude[0][0]
+    swLat = fileData.root.Geolocation.Latitude[0][0]
+    swElevation_m = fileData.root["ASTER GDEM"].ASTGDEM[0][0]
+    swElevation_ft = swElevation_m * 3.28084
+#     print("(", swLat, ", ", swLong, "):\n", swElevation_ft, "ft, ", swElevation_m, "m\n\n")
+            
+    neLong = fileData.root.Geolocation.Longitude[99][99]
+    neLat = fileData.root.Geolocation.Latitude[99][99]
+    neElevation_m = fileData.root["ASTER GDEM"].ASTGDEM[99][99]
+    neElevation_ft = neElevation_m * 3.28084
+#     print("(", neLat, ", ", neLong, "):\n", neElevation_ft, "ft, ", neElevation_m, "m\n\n")
+        
+    return {
+        'sw': {
+            'lat': swLat,
+            'long': swLong,
+            'elevation': swElevation_m
+        },
+        'ne': {
+            'lat': neLat,
+            'long': neLong,
+            'elevation': neElevation_m
+        }
+    }
+
+def addDataToObj(data, obj, fileNum):
+    swIndex = fileNum * 2
+    neIndex = fileNum * 2 + 1
+        
+    obj[swIndex] = {
+        'lat': str(data['sw']['lat']),
+        'long': str(data['sw']['long']),
+        'elevation': str(data['sw']['elevation'])
+    }
+    obj[neIndex] = {
+        'lat': str(data['ne']['lat']),
+        'long': str(data['ne']['long']),
+        'elevation': str(data['ne']['elevation'])
+    }    
+    
+processFiles()
